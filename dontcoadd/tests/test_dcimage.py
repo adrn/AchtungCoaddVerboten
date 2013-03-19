a4d250cd48406d34ea079cecc35637b819a240a8
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-03-19 14:30:27 -0400

    add test for noise model

--- dontcoadd/tests/test_dcimage.py
+++ dontcoadd/tests/test_dcimage.py
@@ -14,7 +14,7 @@ import os, sys
 import numpy as np
 import pytest
 
-from ..dcimage import DCStar, DCImage
+from ..dcimage import DCStar, DCImage, DCGaussianNoiseModel
 
 class TestDCStar(object):
     
@@ -24,5 +24,16 @@ class TestDCStar(object):
         
         star_image_l = star + image
         star_image_r = image + star
+        
+        assert np.all(star_image_l.data == star_image_r.data)
+
+class TestDCGaussianNoiseModel(object):
+    
+    def test_addition(self):
+        nm = DCGaussianNoiseModel(sigma=2.0, sky_level=40.)
+        image = DCImage((10,10), id="derp")
+        
+        star_image_l = nm + image
+        star_image_r = image + nm
         
         assert np.all(star_image_l.data == star_image_r.data)
